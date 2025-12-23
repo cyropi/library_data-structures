4354765df643cbb410ecfbfe8e8a429a02c76716
Ciro Pizza <ciropizza2002@gmail.com>
2025-12-23 23:27:11 +0100

    Update vector.cpp

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,18 +1,21 @@
 
-//#include "vector.hpp"
 #include <algorithm>
 #include <stdexcept>
 
 
 namespace lasd 
 {
-    // (Vector)
+    // Vector
 
     template<typename Data>
     Vector<Data>::Vector(const ulong size)
     {
         this->size = size;
-        this->array = new Data[size];
+
+        if(this->size == 0)
+            this->array = nullptr;
+        else
+            this->array = new Data[size];        
     } 
 
 
@@ -40,7 +43,7 @@ namespace lasd
         std::copy_n(vector.array, this->size, this->array);
     }
 
-    
+
     template<typename Data>
     Vector<Data>::Vector(Vector<Data>&& vector) noexcept 
     {
@@ -52,7 +55,7 @@ namespace lasd
     template<typename Data>
     Vector<Data>::~Vector()
     {
-        delete[] this->array;
+        this->Clear();
     }
 
 
@@ -61,24 +64,9 @@ namespace lasd
     {
         Vector<Data> tmpVector = vector;
         std::swap(tmpVector, *this);
-        delete tmpVector;
 
         return *this; 
     }
-
-
-/*     template<typename Data>
-    Vector<Data>& Vector<Data>::operator=(const Vector<Data>& vector)
-    {   
-        delete[] this->array;
-
-        this->size = vector.size;
-        this->array = new int[this->size];
-
-        std::copy_n(vector.array, this->size, this->array);
-
-        return *this;
-    } */
 
 
     template<typename Data>
@@ -101,27 +89,76 @@ namespace lasd
     template <typename Data>
     bool Vector<Data>::operator!=(const Vector<Data>& vector) const noexcept 
     {
-        return LinearContainer<Data>::operator!=(vector);
+        return !(*this == vector);
     }
 
 
     template<typename Data>
 	const Data& Vector<Data>::operator[](ulong index) const noexcept(false)
     {
-        if(index > this->size)
-            throw std::out_of_range("Indice non valido! Sei fuori dal range della dimensione del vettore...");
+        if(index >= this->size)
+            throw std::out_of_range("Invalid index! You're out of the vector size range...");
         
         return this->array[index];
     }
 
 
     template<typename Data>
+	const Data& Vector<Data>::Front() const noexcept(false)
+    {
+        if(this->Empty())
+            throw std::length_error("Vector is empty! It wasn't possible to fetch the first element...");
+
+        return (*this)[0];
+    }
+
+
+    template <typename Data>
+	const Data& Vector<Data>::Back() const noexcept(false)
+    {
+        if(this->Empty())
+            throw std::length_error("Vector is empty! It wasn't possible to fetch the last element...");
+
+        return (*this)[this->size-1];
+    }
+
+
+    template<typename Data>
 	Data& Vector<Data>::operator[](ulong index) noexcept(false)
     {
-        if(index > this->size)
-            throw std::out_of_range("Indice non valido! Sei fuori dal range della dimensione del vettore...");
+        if(index >= this->size)
+            throw std::out_of_range("Invalid index! You're out of the vector size range...");
         
         return this->array[index];
+    }
+
+
+    template <typename Data>
+    Data& Vector<Data>::Front() noexcept(false)
+    {
+        if(this->Empty())
+            throw std::length_error("Vector is empty! It wasn't possible to fetch the first element...");
+
+        return (*this)[0];
+    }
+
+
+    template <typename Data>
+    Data& Vector<Data>::Back() noexcept(false)
+    {
+        if(this->Empty())
+            throw std::length_error("Vector is empty! It wasn't possible to fetch the last element...");
+
+        return (*this)[this->size-1];
+    }
+
+
+    template<typename Data>
+	void Vector<Data>::Clear()
+    {            
+        delete[] this->array;
+        this->array = nullptr;
+        this->size = 0;
     }
 
 
@@ -130,53 +167,51 @@ namespace lasd
     {
         if(newSize == 0)
         {
-            delete[] this->array;
-            this->array = nullptr;
-            this->size = 0;
-
-            return;
+            this->Clear();
         }
+        else if(this->size != newSize)
+        {
+            Data* tmpArray = new Data[newSize];
 
-        ulong limit = (this->size < newSize) ? this->size : newSize;
+            ulong minSize = (this->size < newSize) ? this->size : newSize;
+            for(ulong i = 0; i < minSize; i++)
+                std::swap(this->array[i], tmpArray[i]); // TODO: modificare con std::move
+            
+            std::swap(this->array, tmpArray);
+            this->size = newSize;
 
-        Data* tmpArray = new Data[newSize];
-
-        for(ulong i = 0; i < limit; i++)
-            std::swap(this->array[i], tmpArray[i]);
-          
-        std::swap(this->array, tmpArray);
-        this->size = newSize;
-
-        delete[] tmpArray;
+            delete[] tmpArray;
+        }
     }
 
 
 
-    /* ************************************************************************** */
-    // (SortableVector)
+
+
+    // SortableVector
 
     template<typename Data>
-    SortableVector<Data>::SortableVector(const ulong size) : Vector<Data>::Vector(size)
+    SortableVector<Data>::SortableVector(const ulong size) : Vector<Data>(size)
     {}
 
 
     template<typename Data>
-    SortableVector<Data>::SortableVector(const TraversableContainer<Data>& traversableContainer) : Vector<Data>::Vector(traversableContainer)
+    SortableVector<Data>::SortableVector(const TraversableContainer<Data>& traversableContainer) : Vector<Data>(traversableContainer)
     {}
 
 
     template<typename Data>
-    SortableVector<Data>::SortableVector(MappableContainer<Data>&& mappableContainer) : Vector<Data>::Vector(std::move(mappableContainer))
+    SortableVector<Data>::SortableVector(MappableContainer<Data>&& mappableContainer) : Vector<Data>(std::move(mappableContainer))
     {}
 
 
     template<typename Data>
-    SortableVector<Data>::SortableVector(const SortableVector<Data>& sortableVector) : Vector<Data>::Vector(sortableVector)
+    SortableVector<Data>::SortableVector(const SortableVector<Data>& sortableVector) : Vector<Data>(sortableVector)
     {}
 
 
     template<typename Data>
-    SortableVector<Data>::SortableVector(SortableVector<Data>&& sortableVector) : Vector<Data>::Vector(std::move(sortableVector))
+    SortableVector<Data>::SortableVector(SortableVector<Data>&& sortableVector) : Vector<Data>(std::move(sortableVector))
     {}
 
 
